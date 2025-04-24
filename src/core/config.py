34ef918d6f27dcafd08b28eb3ed54ce2e54fc240
Gor903 <gor.beglaryan.rw@gmail.com>
2025-04-24 16:18:31 +0400

    Update: domain name in settings

--- src/core/config.py
+++ src/core/config.py
@@ -10,6 +10,8 @@ load_dotenv()
 class Settings(BaseSettings):
     PROJECT_NAME: str = "FastAPI-Abstract"
 
+    DOMAIN_NAME: str = os.getenv("DOMAIN_NAME")
+
     SECRET_KEY: str = os.getenv("SECRET_KEY", "SuperSecretKey")
 
     ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 15))
@@ -32,9 +34,13 @@ class Settings(BaseSettings):
         + f"{os.getenv('POSTGRES_DB')}"
     )
 
-    CELERY_BROKER_URL: str = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0")
+    CELERY_BROKER_URL: str = os.getenv(
+        "CELERY_BROKER_URL",
+        "redis://redis:6379/0"
+    )
     CELERY_RESULT_BACKEND: str = os.getenv(
-        "CELERY_RESULT_BACKEND", "redis://localhost:6379/1"
+        "CELERY_RESULT_BACKEND",
+        "redis://redis:6379/1"
     )
 
     SMTP_HOST: str = os.getenv("SMTP_HOST")
--- src/tasks/email.py
+++ src/tasks/email.py
@@ -1,10 +1,12 @@
 from celery import shared_task
-from src.utils.mailer import send_email  # use your actual email function
+
+from src.utils.mailer import send_email
+from src.core import settings
 
 
 @shared_task
 def send_verification_email_task(email: str, token: str):
-    url = f"http://yourdomain.com/verify-email?token={token}"
+    url = f"http://{settings.DOMAIN_NAME}/auth/verify-email?token={token}"
     subject = "Verify your email"
     body = f"Click the link to verify your email:\n\n{url}"
     send_email(to=email, subject=subject, body=body)
